9fd371e337e5b0e7368ef75ab92d8a70c820232b
VENKAT-JAN26 <venkatklsr@gmail.com>
2024-12-11 08:18:05 +0530

    sidebar fixed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,28 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
+import "./Home.css";
+import PropTypes from "prop-types";
+import Feeds from "../../Components/Feed/feed";
+import Sidebar1 from "../../Components/Sidebar/sidebar";
 
-
-
-import Sidebar from '../../Components/Sidebar/sidebar';
-import './Home.css';
-import PropTypes from 'prop-types';
-import Feeds from '../../Components/Feed/feed';
-
-const Home = ({sidebar}) => {
-
-   const [category , setCategory] = useState(0);
+const Home = ({ sidebar }) => {
+  const [category, setCategory] = useState(0);
 
   return (
     <>
       {/* Passing the sidebar prop to the Sidebar component */}
-      <Sidebar sidebar={sidebar} category={category} setCategory={setCategory} />
+      <Sidebar1
+        sidebar={sidebar}
+        category={category}
+        setCategory={setCategory}
+      />
 
-      <div className={`container ${sidebar?"":'largecontainer'}`}>
-        <Feeds category={category} setCategory={setCategory}/>
+      <div className={`container ${sidebar ? "" : "largecontainer"}`}>
+        <Feeds category={category} setCategory={setCategory} />
       </div>
     </>
   );
 };
-
 
 // Add propTypes validation
 Home.propTypes = {
